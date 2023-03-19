d2f44ffc62fac940506d8152d457b47fc5214c8d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-19 09:47:57 +0300

    header componentt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import "./App.css";
-
+import Header from "./Component/Layouts/Header";
+import Footer from "./Component/Layouts/Footer";
 function App() {
   return (
     <div className="App">
       <h1>Shopt IT</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/Component/Layouts/Header.js
+++ src/Component/Layouts/Header.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-
+import '../../App.css'
 export default function Header() {
   return (
     <Fragment>

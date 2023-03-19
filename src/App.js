0677b97c5c3599341a2b7891129e4bde4563996c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-19 09:58:06 +0300

    home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import "./App.css";
 import Header from "./Component/Layouts/Header";
 import Footer from "./Component/Layouts/Footer";
+import Home from "./Component/Home";
 function App() {
   return (
     <div className="App">
       <h1>Shopt IT</h1>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }

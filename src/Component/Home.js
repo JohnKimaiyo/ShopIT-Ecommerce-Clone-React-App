4d8a529167d31ee7f94c03a230d1bac1a13e6b16
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-19 12:18:34 +0300

    loader compoenent

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,8 +1,72 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
-export default function Home() {
+import MetaData from "./layout/MetaData";
+import Product from "./product/Product";
+import Loader from "./layout/Loader";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import { getProducts } from "../actions/productActions";
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
+const Home = ({ match }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
+  const [category, setCategory] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const categories = [
+    "Electronics",
+    "Cameras",
+    "Laptops",
+    "Accessories",
+    "Headphones",
+    "Food",
+    "Books",
+    "Clothes/Shoes",
+    "Beauty/Health",
+    "Sports",
+    "Outdoor",
+    "Home",
+  ];
+
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const {
+    loading,
+    products,
+    error,
+    productsCount,
+    resPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
+
+  const keyword = match.params.keyword;
+
+  useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
+
+    dispatch(getProducts(keyword, currentPage, price, category, rating));
+  }, [dispatch, alert, error, keyword, currentPage, price, category, rating]);
+
+  function setCurrentPageNo(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
+  let count = productsCount;
+  if (keyword) {
+    count = filteredProductsCount;
+  }
+
   return (
-    
     <Fragment>
       {loading ? (
         <Loader />
@@ -122,4 +186,6 @@ export default function Home() {
       )}
     </Fragment>
   );
-}
+};
+
+export default Home;
